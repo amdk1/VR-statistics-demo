7d5f30249eef3c78b097f51ea75b0d07dcb4bbe7
amdk1 <100428619+amdk1@users.noreply.github.com>
2024-03-21 11:21:37 +0300

    Add files via upload

---
+++ RunTrial.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class RunTrial : MonoBehaviour
+{
+    public float chance = 1;
+    public int trials = 5;
+    public GameObject leftPos;
+    public GameObject rightPos;
+    public float[] outcomes;
+    public int repetitions = 40;
+    public float successfulDrops = 0;
+    public float unsuccessfulDrops = 0;
+    int initialReps;
+    int initialTrials;
+    Vector3 offset = new Vector3(0.001f, 0.001f, 0.001f);
+    // Start is called before the first frame update
+
+    void Start() 
+    {
+        Directory.CreateDirectory(Application.streamingAssetsPath + "/Obtained_Values/");
+        initialReps = repetitions;
+        initialTrials = trials;
+        outcomes = new float[initialReps];
+
+        for (int i = 0; i < (repetitions*1000); i++)
+        {
+            while (trials > 0)
+            {
+                float randomNum = Random.Range(0f, 1f);
+                if (randomNum <= chance)
+                {
+                    successfulDrops++;
+                    GameObject item = Instantiate(leftPos) as GameObject;
+                    item.transform.parent = transform;
+                    item.transform.position = leftPos.transform.position + offset;
+                }
+                else
+                {
+                    unsuccessfulDrops++;
+                    GameObject item = Instantiate(rightPos) as GameObject;
+                    item.transform.parent = transform;
+                    item.transform.position = rightPos.transform.position + offset;
+                }
+                trials--;               
+            }
+            outcomes[initialReps - repetitions] = successfulDrops;
+            trials = initialTrials;
+            successfulDrops = 0;
+            repetitions--;            
+        }
+        CreateFile();
+    }
+
+    public void CreateFile()
+    {
+        string docName = Application.streamingAssetsPath + "/Obtained_Values/" + "Obtained" + ".txt";
+
+        if (!File.Exists(docName))
+        {
+            File.WriteAllText(docName, "VALUES POG \n\n");
+        }
+        
+        for(int i = 0; i < initialReps; i++)
+        {
+            File.AppendAllText(docName, outcomes[i].ToString() + ",");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
